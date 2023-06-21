37a10691389ad24ec495abb3003a873e3e6aa545
Cristiana Vaculovschi <110521123+wakulovszki@users.noreply.github.com>
2023-06-20 18:32:10 -0700

    updates

--- ASG/main.tf
+++ ASG/main.tf
@@ -20,9 +20,39 @@ module "asg" {
   instance_type     = "t3.micro"
   ebs_optimized     = true
   enable_monitoring = true
+
 }
 
 
+resource "aws_security_group" "lb-firewall" {
+  name        = "lb-firewall"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 
 resource "aws_elb" "bar" {
   name = "foobar-terraform-elbs"
