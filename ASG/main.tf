4129cac828f4dda8605ef326263337beda113d3d
Cristiana Vaculovschi <110521123+wakulovszki@users.noreply.github.com>
2023-06-20 18:24:38 -0700

    updates

--- ASG/main.tf
+++ ASG/main.tf
@@ -9,7 +9,7 @@ module "asg" {
   desired_capacity          = 3
   wait_for_capacity_timeout = 0
   health_check_type         = "EC2"
-  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.private.subnets
+  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.public_subnets
 
   # Launch template
   launch_template_name        = "example-asg"
@@ -20,4 +20,37 @@ module "asg" {
   instance_type     = "t3.micro"
   ebs_optimized     = true
   enable_monitoring = true
+}
+
+
+
+resource "aws_elb" "bar" {
+  name = "foobar-terraform-elbs"
+  availability_zones = data.terraform_remote_state.vpc.outputs.azs
+  security_groups = [aws_security_group.asg-sec-group.id]
+
+
+listener {
+instance_port = 80
+instance_protocol = "http"
+lb_port = 80
+lb_protocol = "http"
+}
+
+
+health_check {
+healthy_threshold = 2
+unhealthy_threshold = 2
+timeout = 3
+target = "TCP:80"
+interval = 30
+}
+
+cross_zone_load_balancing = true
+idle_timeout = 400
+connection_draining = true
+connection_draining_timeout = 400
+
+
+
 }
